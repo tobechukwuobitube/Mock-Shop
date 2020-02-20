d926c1ab6c3180ef63c29b09b61f17a1396fc655
tobechukwuobitube <tobechukwuobitube@gmail.com>
2020-02-21 00:04:52 +0100

    Setup admin add product

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import UsersRoutes from './users';
+import productRoutes from './products';
 
 const app = express();
 
 app.use('/api/v1/', UsersRoutes);
+app.use('/api/v1/', productRoutes);
 
 export default app;
